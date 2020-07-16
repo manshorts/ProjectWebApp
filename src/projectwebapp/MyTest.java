174e67ad4fa9be0d8b52f4df5af3c0841c35e73a
manshorts <sdonner@byui.edu>
2020-07-16 12:53:22 -0700

    Added comments

--- src/projectwebapp/MyTest.java
+++ src/projectwebapp/MyTest.java
@@ -9,17 +9,13 @@ public class MyTest {
     @Test
     public void collectionEquals(List webCollection, List databaseCollection) {
         System.out.println("Testing collections to ensure that same.");
+        System.out.println("If no assertion error then collections are same.");
+        // goes through each object in each collection to check if same.
         for (Object game : webCollection) {
             for (Object object : databaseCollection) {
+                // checks each individual object
                 assertEquals(game, object);
             }
         }
     }
-
-    @Test
-    public void objectEquals(Object game, Object object) {
-        System.out.println("Testing each object to ensure that same.");
-        assertEquals(game, object);
-    }
-
 }
